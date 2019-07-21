5df7ef311ab2860297179fd5f374a5aae428e2f7
Anitha987 <anithaumuhire@gmail.com>
2019-07-21 22:00:30 +0200

    add paragraph elements

--- file.js/java.js
+++ file.js/java.js
@@ -56,3 +56,11 @@ else if (Result==6 && ge=="male"){
  return know;
 }
 
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
+
