f80bae1f62a791fbd9a5233ae264a8486a5b539f
Anitha987 <anithaumuhire@gmail.com>
2019-07-22 08:16:55 +0200

    remove function elements

--- file.js/java.js
+++ file.js/java.js
@@ -56,11 +56,4 @@ else if (Result==6 && ge=="male"){
  return know;
 }
 
-function openNav() {
-  document.getElementById("myNav").style.height = "100%";
-}
-
-function closeNav() {
-  document.getElementById("myNav").style.height = "0%";
-}
 
